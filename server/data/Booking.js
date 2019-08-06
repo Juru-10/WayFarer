d14c08611b09ba7286f395503a57d3ddeb9cd6e7
Juru-10 <jurassu10@gmail.com>
2019-08-07 00:30:23 +0200

    feature(delete-booking):add a booking delete feature
    -add the endpoint to delete a booking
    
    [Delivers #167746638]

--- server/data/Booking.js
+++ server/data/Booking.js
@@ -6,6 +6,13 @@ const Booking = {
   findAll() {
     return this.bookings;
   },
+
+  delete(id) {
+    const booking = this.findOne(id);
+    const index = this.bookings.indexOf(booking);
+    this.bookings.splice(index, 1);
+    return {};
+  }
   
 }
 export default Booking;
