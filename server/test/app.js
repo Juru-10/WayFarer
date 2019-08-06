6ab6906c745d99f42acafac8742a967cba598983
Juru-10 <jurassu10@gmail.com>
2019-08-06 09:34:12 +0200

    add the backend folder structure

--- server/test/app.js
+++
@@ -1,7 +0,0 @@
-const expect = require('chai').expect;
-
-describe('A basic test', function (){
-  it('should pass when everything is  okay', function (){
-      expect(true).to.be.true;
-  });
-});
